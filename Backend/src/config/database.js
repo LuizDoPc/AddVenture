4409ef963ab2046caf8ceba6a97d6bb6dad8f9c3
luizdopc <luizotaviosoares@outlook.com>
2019-12-04 00:02:54 -0300

    update database credentials

--- Backend/src/config/database.js
+++ Backend/src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: "postgres",
-  host: "0.0.0.0",
-  username: "postgres",
-  password: "toor",
-  database: "sqlnode",
+  host: "ec2-54-243-47-196.compute-1.amazonaws.com",
+  username: "vapedhxvkccgys",
+  password: "6ffc534833ab051c793b327f6d38bc7135d68c200e224be1215639ca95a7f466",
+  database: "df8je49nh2c3cv",
   define: {
     timestamps: true,
     underscored: true
